d1be2a3458391befc7d82fa63457389e511d7535
Mohammed-Ashraful <mr.ashraful2020@gmail.com>
2021-10-28 18:44:15 +0600

    This is ema-jhon

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Ema jon server is running');  
+  res.send('Ema jon server is running on heroku');  
 });
 
 app.listen(port, () => {
